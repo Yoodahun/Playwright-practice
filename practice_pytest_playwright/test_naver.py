f3ae6346a342a09c0f38715c51a2a57477a2d292
yoodahun <dahun4032@gmail.com>
2024-03-03 21:14:55 +0900

    update assert logic

--- practice_pytest_playwright/test_naver.py
+++ practice_pytest_playwright/test_naver.py
@@ -1,4 +1,5 @@
 import re
+import pytest
 
 from playwright.sync_api import Page, expect
 
@@ -8,4 +9,4 @@ class TestNaver:
     def test_naver_home(self, page:Page):
 
         page.goto("http://www.naver.com", wait_until="load")
-        expect(page.locator("a").filter(has_text=re.compile(r"^NAVER$"))).to_be_visible()
+        assert page.locator("a").filter(has_text=re.compile(r"^NAVER$")).is_hidden()
