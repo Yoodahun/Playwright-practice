2c4ca20ec351cd3fa8938832389e6e1cd64cab50
yoodahun <dahun4032@gmail.com>
2024-03-06 20:27:36 +0900

    add testcase

--- practice_pytest_playwright/test_naver.py
+++ practice_pytest_playwright/test_naver.py
@@ -19,3 +19,10 @@ class TestNaver:
         # assert page.locator("a").filter(has_text=re.compile(r"^NAVER$")).is_visible()
 
         expect(page.locator("a").filter(has_text=re.compile(r"^NAVER$"))).to_be_visible()
+
+    def test_naver_home_with_success(self, page:Page):
+
+        page.goto("http://www.naver.com", wait_until="load")
+        # assert page.locator("a").filter(has_text=re.compile(r"^NAVER$")).is_visible()
+
+        expect(page.locator("a").filter(has_text=re.compile(r"^NAVER$"))).to_be_hidden()
